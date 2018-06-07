f16a350d900f07c103e6ea232d62bb2b4a988dab
Elias Kosunen <elias.kosunen@gmail.com>
2018-06-07 19:52:43 +0300

    Add up-to-date clang install to prereqs-apt

--- prereqs-apt.sh
+++ prereqs-apt.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y git curl wget zsh libclang-dev build-essential cmake python-dev python3-dev vim-gtk3 openjdk-8-jdk openjdk-8-jre golang-go
+sudo apt-get install -y git curl zsh build-essential cmake python-dev python3-dev vim-gtk3 openjdk-8-jdk openjdk-8-jre golang-go
+
+. /etc/os-release
+
+# clang
+if [ "$ID" = "debian" ]; then
+    echo "deb https://apt.llvm.org/unstable/ llvm-toolchain main" | sudo tee /etc/apt/sources.list.d/llvm.list
+    echo "deb-src https://apt.llvm.org/unstable/ llvm-toolchain main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
+    echo "deb https://apt.llvm.org/unstable/ llvm-toolchain-5.0 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
+    echo "deb-src https://apt.llvm.org/unstable/ llvm-toolchain-5.0 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
+    echo "deb https://apt.llvm.org/unstable/ llvm-toolchain-6.0 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
+    echo "deb-src https://apt.llvm.org/unstable/ llvm-toolchain-6.0 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
+else
+    echo "deb https://apt.llvm.org/${UBUNTU_CODENAME}/ llvm-toolchain-${UBUNTU_CODENAME} main" | sudo tee /etc/apt/sources.list.d/llvm.list
+    echo "deb-src https://apt.llvm.org/${UBUNTU_CODENAME}/ llvm-toolchain-${UBUNTU_CODENAME} main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
+    echo "deb https://apt.llvm.org/${UBUNTU_CODENAME}/ llvm-toolchain-${UBUNTU_CODENAME}-5.0 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
+    echo "deb-src https://apt.llvm.org/${UBUNTU_CODENAME}/ llvm-toolchain-${UBUNTU_CODENAME}-5.0 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
+    echo "deb https://apt.llvm.org/${UBUNTU_CODENAME}/ llvm-toolchain-${UBUNTU_CODENAME}-6.0 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
+    echo "deb-src https://apt.llvm.org/${UBUNTU_CODENAME}/ llvm-toolchain-${UBUNTU_CODENAME}-6.0 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
+fi
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo apt-get -y install clang libclang-dev clang-tools
 
 # node, typescript
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
@@ -9,17 +30,15 @@ sudo apt-get install -y nodejs
 sudo npm install -g typescript ts-server
 
 # mono
-sudo apt install dirmngr || true
+sudo apt-get install apt-transport-https dirmngr || true
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-sudo apt install apt-transport-https || true
-. /etc/os-release
 if [ "$ID" = "debian" ]; then
     echo "deb https://download.mono-project.com/repo/debian stable-stretch main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 else
     echo "deb https://download.mono-project.com/repo/ubuntu stable-${UBUNTU_CODENAME} main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 fi
-sudo apt update
-sudo apt install -y mono-complete
+sudo apt-get update
+sudo apt-get install -y mono-complete
 
 # rust
 curl https://sh.rustup.rs -sSf | sh
